0e0b30e62eeab4b4698aaab1498c8b35247efd4f
morphy418 <z.harshegyi@gmail.com>
2020-12-06 11:45:24 +0000

    finish final touches of responsiveness

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-
-$('.riddle-button').click(function() {
+$('.riddle-button').click(function () {
   alert('Yes, the answer is REFLECTION, well done! Now enjoy your well-deserved 10% discount and call me now on 07548637316!');
 });
 
 
-$(".fab.fa-facebook").on('click', function(){
-  window.location = "https://www.facebook.com/KeeweeAcademy";    
+$(".fab.fa-facebook").on('click', function () {
+  window.location = "https://www.facebook.com/KeeweeAcademy";
 });
 
-$(".fas.fa-envelope").on('click', function(){
-  window.location = "mailto: zoltan@echofriends.org.uk";    
+$(".fas.fa-envelope").on('click', function () {
+  window.location = "mailto: zoltan@echofriends.org.uk";
 });
 
-$("p.phone").on('click', function(){
-  window.open("tel:+447548637316");    
+$("p.phone").on('click', function () {
+  window.open("tel:+447548637316");
 });
